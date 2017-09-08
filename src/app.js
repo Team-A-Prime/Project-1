c2dff037a83a329940a8ea9cf48769a068125c64
Jacob Hegna <jacobhegna@gmail.com>
2017-09-08 18:23:30 -0500

    Add Event class
    This commit adds an event class to the app, and removes the old
    event logic from the database class.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const app = express();
+const express       = require('express');
+const app           = express();
 
-const Database = require('./database.js');
+const Database      = require('./database.js');
+const Event         = require('./event.js');
 
-var test_data = {
-    name: "Georgia State University",
-    description: "Debate tournament",
-    time_slots: [0, 1, 2, 5, 46],
-    attendees: ["Jacob", "Henry"]
-};
+
+var test_event = new Event();
+test_event.name = "Georgia State University";
+test_event.description = "Debate tournament";
+test_event.time_slots = [0, 1, 2, 5, 46];
+test_event.attendees = ["Jacob", "Henry"];
 
 app.get('/', function(req, res) {
     res.send('Hello, world!');
 });
 
 var db = new Database('test.db');
-db.write_event(test_data);
+db.write_event(test_event);
 
 app.listen(8080, function() {
     console.log('Example app listening on port 8080!');
--- src/database.js
+++ src/database.js
@@ -6,8 +6,6 @@
 
 var sqlite3 = require('sqlite3').verbose();
 
-var crypto = require('crypto');
-
 /**
  * Database(path)
  * @pre: nothing
@@ -31,67 +29,21 @@ function Database(path) {
 } // end of function Database
 
 /**
- * verify_event(event)
- * @pre: nothing
- * @post: nothing
- * @return: true if the 'event' param meets the requirements for an event object
- * @param: 'event', the object to be checked for validity
- */
-Database.prototype.verify_event = function(event) {
-    /* The structure event needs is:
-     * name: string
-     * description: string
-     * time_slots: array
-     * attendees: array
-     * Note: We can't verify the types of these fields, just their existence
-     */
-
-     // taken from https://stackoverflow.com/questions/135448/how-do-i-check-if-an-object-has-a-property-in-javascript
-     let has = function(obj, key) {
-        return obj ? hasOwnProperty.call(obj, key) : false;
-     };
-
-     return (  has(event, 'name')
-            && has(event, 'description')
-            && has(event, 'time_slots')
-            && has(event, 'attendees')
-            );
-} // end of function verify_event
-
-Database.prototype.hash_event = function(event) {
-    if(!this.verify_event(event)) {
-        return false;
-    }
-
-    let sha256 = crypto.createHash('sha256');
-    sha256.update(event.name);
-    event.time_slots.forEach(function(ele, idx) {
-        sha256.update("" + ele);
-    });
-
-    return sha256.digest('hex');
-}
-
-/**
- * write_event(event)
+ * Database#write_event(event)
  * @pre: the db is initialized properly
  * @post: event is written to the db
  * @return: nothing
  * @param: 'event', the object to be written to the db
  */
 Database.prototype.write_event = function(event) {
-    if(!this.verify_event(event)) {
-        return;
-    }
-
     this.db.run("INSERT INTO tb_events (hash, name, description, time_slots, attendees) VALUES"
               + "("
-              + '\'' + this.hash_event(event).substring(0, 8) + '\', '
+              + '\'' + event.hash().substring(0, 8) + '\', '
               + '\'' + event.name + '\', '
               + '\'' + event.description + '\', '
               + '\'' + event.time_slots + '\', '
               + '\'' + event.attendees + '\''
               + ");");
-} // end of function write_event
+} // end of function Database#write_event
 
 module.exports = Database;
