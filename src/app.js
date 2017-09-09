5bc503ddab637834a2d37c9c45bfa35ba2db48df
Jacob Hegna <jacobhegna@gmail.com>
2017-09-08 22:21:45 -0500

    Clean up app.js
    There was previously some test code in app.js, but due to the
    addition of unit tests, that code can now be removed

--- src/app.js
+++ src/app.js
@@ -5,19 +5,8 @@ const Database      = require('./database.js');
 const Event         = require('./event.js');
 
 
-var test_event = new Event();
-test_event.name = "Georgia State University";
-test_event.description = "Debate tournament";
-test_event.time_slots = [0, 1, 2, 5, 46];
-test_event.attendees = ["Jacob", "Henry"];
-
 app.get('/', function(req, res) {
-    res.send('Hello, world!');
+    res.send('EECS 448 Project 1');
 });
 
-var db = new Database('test.db');
-db.write_event(test_event);
-
-app.listen(8080, function() {
-    console.log('Example app listening on port 8080!');
-});
+app.listen(8080, function() {});
