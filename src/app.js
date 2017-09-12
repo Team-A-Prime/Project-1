502ba3ea8ca96022b2a8c3a35fe022c024e92399
Jacob Hegna <jacobhegna@gmail.com>
2017-09-12 13:50:14 -0500

    Clean up app.js
    Formatted app.js more cleanly, added an anonymous main function

--- src/app.js
+++ src/app.js
@@ -1,12 +1,54 @@
+/**
+ * @file: app.js
+ * @authors: jacob hegna <jacobhegna@gmail.com
+ * @date: September 2017
+ * @brief: Initializationa nd entry point of the server
+ */
+
 const express       = require('express');
-const app           = express();
+const bodyParser    = require('body-parser');
 
 const Database      = require('./database.js');
 const Event         = require('./event.js');
 
+/**
+ * Anonymous main function
+ * @pre: nothing
+ * @post: the server has stopped running
+ * @return: nothing
+ */
+/* main */ (() => {
 
-app.get('/', function(req, res) {
-    res.send('EECS 448 Project 1');
-});
+    let app      = express();
+    let database = new Database('test.db');
 
-app.listen(8080, function() {});
+    // Set up body-parser in the Express app
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    // Set up routing
+    app.use(express.static('./public'))
+
+    // API for creating a new event
+    app.post('/api/events/new', function(req, res) {
+        let event = new Event();
+
+        event.name        = req.body.name;
+        event.description = req.body.description;
+        event.time_slots  = JSON.parse(req.body.time_slots);
+        event.attendees   = JSON.parse(req.body.attendees);
+
+        res.redirect('/');
+    });
+
+    // API for getting the current list of events
+    app.get('/api/events', function(req, res) {
+        database.read_events(function(events) {
+            res.send(events);
+        });
+    });
+
+    // Start the server
+    app.listen(8080);
+
+})(); // end of anononymous main
